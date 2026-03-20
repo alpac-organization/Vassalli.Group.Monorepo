f3d9a89b5fa88eeaed425cf0124272c91bcd2a6a
Gelzint vidaure <vidaureanto2002@gmail.com>
2026-03-20 17:41:48 -0600

    name changed

--- apps/erp-alpac-group/src/routers/main-routes.tsx
+++ apps/erp-alpac-group/src/routers/main-routes.tsx
@@ -1,6 +1,6 @@
 import { LoginPage } from "@app/modules/auth/login/ui/login-page";
 import { SelectCompany } from "@app/modules/auth/select-company/ui/select-company";
-import Dashboard from "@app/modules/dashboard/Dashboard";
+import Dashboard from "@app/modules/dashboard/Dashboard1";
 import type { RouteObject } from "react-router-dom";
 
 export const MainRoutes: RouteObject[] = [
