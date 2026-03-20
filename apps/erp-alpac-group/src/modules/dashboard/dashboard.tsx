28c0c81cf6c84fd3c50887ef3a27517f63fe30af
Gelzint vidaure <vidaureanto2002@gmail.com>
2026-03-20 16:18:25 -0600

    refactor relative routes dashboard-cards

--- apps/erp-alpac-group/src/modules/dashboard/dashboard.tsx
+++ apps/erp-alpac-group/src/modules/dashboard/dashboard.tsx
@@ -1,4 +1,4 @@
-import { useTheme } from "@alpac/design-system";
+import { useTheme } from "../../../../../packages/ui/src/providers/theme-provider";
 import { DashBoardCard } from "../../../../../packages/ui/src/components/cardDashboard/DashboardCard";
 import { templatesData } from "./data/templates.data";
 import { useId } from "react";
