cfb8f61b1cf0ad1154c611f65fc755c8580f54c2
Desarrollo <desarrollo@jvco.com.ni>
2026-03-27 17:21:49 -0600

    refactor: limpiar codigo comentado

--- apps/erp-alpac-group/src/modules/payroll/ui/pages/collaborator-index/collaborator.page.types.ts
+++ apps/erp-alpac-group/src/modules/payroll/ui/pages/collaborator-index/collaborator.page.types.ts
@@ -1,12 +1,3 @@
-/*   const data = [
-        { id: "001", fullName: "Juan Pérez", position: "Desarrollador Senior", department: "Tecnología", status: "Activo", showProfile: "Ver Perfil" },
-        { id: "002", fullName: "María García", position: "Analista de RRHH", department: "Recursos Humanos", status: "Activo", showProfile: "Ver Perfil" },
-        { id: "003", fullName: "Carlos Rodriguez", position: "Gerente de Ventas", department: "Comercial", status: "Vacaciones", showProfile: "Ver Perfil" },
-        { id: "004", fullName: "Lucía Méndez", position: "Diseñadora UX", department: "Producto", status: "Activo", showProfile: "Ver Perfil" },
-        { id: "005", fullName: "Roberto Sánchez", position: "Contador", department: "Finanzas", status: "Baja", showProfile: "Ver Perfil" },
-    ]
- */
-
 export type Collaborator = {
     id: string
     fullName: string
