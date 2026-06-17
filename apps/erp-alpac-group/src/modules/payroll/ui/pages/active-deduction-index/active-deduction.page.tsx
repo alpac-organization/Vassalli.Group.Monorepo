582efe53f4ef05248851dc8dfe9720697217e5c1
Desarrollo <desarrollo@jvco.com.ni>
2026-06-17 09:27:45 -0600

    refactor: quitar boton de accion en deducciones activas

--- apps/erp-alpac-group/src/modules/payroll/ui/pages/active-deduction-index/active-deduction.page.tsx
+++ apps/erp-alpac-group/src/modules/payroll/ui/pages/active-deduction-index/active-deduction.page.tsx
@@ -209,27 +209,6 @@ export const ActiveDeductionsPage = () => {
 
         <div className="flex justify-between items-center pt-4 border-t border-t-slate-600 dark:border-t-neutral-600">
           <div className="flex flex-col justify-center">
-            <h3 className="p-0! m-0!">Accesos Directos</h3>
-            <small className="text-gray-500 dark:text-gray-300">
-              Filtra por identificación o tipo de deducción
-            </small>
-          </div>
-        </div>
-
-        <div className="w-full dark:bg-[#272b34]! p-4 rounded-md border border-slate-600 dark:border-neutral-600">
-          <div className="w-full flex flex-col gap-4 md:flex-row md:flex-wrap md:items-center md:justify-start">
-            <Button
-              size="giant"
-              label="Acción 1"
-              icon={<ArrowRight size={20} />}
-              className="w-full! md:w-auto! text-[15px]! rounded-md! text-white! bg-alpac-primary-500! dark:bg-alpac-primary-700!"
-              onClick={() => console.log("testing")}
-            />
-          </div>
-        </div>
-
-        <div className="flex justify-between items-center pt-4 border-t border-t-slate-600 dark:border-t-neutral-600">
-          <div className="flex flex-col justify-center">
             <h3 className="p-0! m-0!">Filtros</h3>
             <small className="text-gray-500 dark:text-gray-300">
               Filtra por identificación o tipo de deducción
