993d175436715a7ae73d95e165d82ea57b242f32
Desarrollo <desarrollo@jvco.com.ni>
2026-06-26 10:13:24 -0600

    fix: ajustes menores

--- apps/erp-alpac-group/src/modules/payroll/ui/pages/nomina/components/incomes/create-income-modal/create-income-modal.tsx
+++ apps/erp-alpac-group/src/modules/payroll/ui/pages/nomina/components/incomes/create-income-modal/create-income-modal.tsx
@@ -30,7 +30,6 @@ export const CreateIncomeModal = ({
       <LazyMotion features={loadFeatures} strict>
         <div className="flex flex-col gap-5">
           <AnimatePresence>
-            {
               <m.div
                 key="subsidy-form"
                 initial={{ opacity: 0, y: 16, height: 0, overflow: "hidden" }}
@@ -66,7 +65,6 @@ export const CreateIncomeModal = ({
                   />
                 </div>
               </m.div>
-            }
           </AnimatePresence>
         </div>
       </LazyMotion>
