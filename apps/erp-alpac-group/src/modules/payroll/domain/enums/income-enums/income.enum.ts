2ec99b9a53a2f406c194a4a5c6f638f23021d9d2
Desarrollo <desarrollo@jvco.com.ni>
2026-06-29 18:38:08 -0600

    feat: registros de ingreso depreciacion

--- apps/erp-alpac-group/src/modules/payroll/domain/enums/income-enums/income.enum.ts
+++ apps/erp-alpac-group/src/modules/payroll/domain/enums/income-enums/income.enum.ts
@@ -2,6 +2,7 @@ export const IncomeTypeEnum = {
    INCOME_OVERTIME: "OVERTIME",
    INCOME_COMMISSION: "COMMISSION",
    INCOME_BONUS: "BONUS",
+   INCOME_DEPRECIATION: "DEPRECIATION"
 } as const;
 
 export type IncomeTypeEnum = (typeof IncomeTypeEnum)[keyof typeof IncomeTypeEnum];
