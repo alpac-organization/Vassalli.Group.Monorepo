6e81493c9cfb140fa277bd78c9c2dc99a38f619a
Donald Jose Munguia Guadamuz <jdonaldmunguia@gmail.com>
2026-07-07 16:06:08 -0600

    refactor: se quita comentarios

--- apps/erp-alpac-group/src/modules/payroll/ui/pages/attendance-control/components/attendance-control-table/attendance-control-table.tsx
+++ apps/erp-alpac-group/src/modules/payroll/ui/pages/attendance-control/components/attendance-control-table/attendance-control-table.tsx
@@ -3,48 +3,6 @@ import type { AttendanceRecordDto } from "@app/modules/payroll/domain/ApiContrac
 import { formatDateToSpanishWords } from "@app/shared/utils/string.utils";
 import type { AttendanceControlTableProps } from "./attendance-control-table.types";
 
-/*{
-    "data": [
-        {
-            "user_id": 18,
-            "date": "2026-05-15T00:00:00",
-            "identification_number": "0012210790054K",
-            "collaborator_fullname": "Alba Encarnacion Gutierrez Morales",
-            "markings": [
-                {
-                    "read_time": "2026-05-15T08:09:55",
-                    "device_name": "ALPAC-MGA"
-                },
-                {
-                    "read_time": "2026-05-15T17:10:57",
-                    "device_name": "ALPAC-MGA"
-                }
-            ]
-        },
-        {
-            "user_id": 18,
-            "date": "2026-05-14T00:00:00",
-            "identification_number": "0012210790054K",
-            "collaborator_fullname": "Alba Encarnacion Gutierrez Morales",
-            "markings": [
-                {
-                    "read_time": "2026-05-14T08:11:05",
-                    "device_name": "ALPAC-MGA"
-                },
-                {
-                    "read_time": "2026-05-14T17:02:31",
-                    "device_name": "ALPAC-MGA"
-                }
-            ]
-        },
-    ],
-    "page_number": 1,
-    "page_size": 10,
-    "total": 10
-}*/
-
-
-
 export const AttendanceControlTable = ({ data, pagination, onSelect }: AttendanceControlTableProps) => {
 
    const columns: TableColumn<AttendanceRecordDto>[] = [
