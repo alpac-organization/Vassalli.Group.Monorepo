e0e107d9dc80a5438028b6c4f6a26a92fa2329fe
Angel-Delg <lariosangel062@gmail.com>
2026-08-14 16:52:29 -0600

    feat: guardar cambios

--- apps/erp-alpac-group/src/App.tsx
+++ apps/erp-alpac-group/src/App.tsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client"
 import { QueryClient } from "@tanstack/react-query"
 import { createBrowserRouter } from "react-router-dom"
+import { registerSW } from "virtual:pwa-register"
 
 import Main from "@app/main"
 import { MainRouter } from "@app/routers/main-router"
@@ -8,6 +9,8 @@ import { DatePickerProvider, ThemeProvider } from "@alpac/design-system"
 import { InactivityProvider } from "@app/shared/providers/inactivity-provider"
 import { MotionConfig } from "framer-motion"
 import { withErrorElement } from "./shared/components/error-boundary/error-boundary.helper"
+
+registerSW({ immediate: true })
 
 const queryClient = new QueryClient()
 const router = createBrowserRouter(withErrorElement(MainRouter));
