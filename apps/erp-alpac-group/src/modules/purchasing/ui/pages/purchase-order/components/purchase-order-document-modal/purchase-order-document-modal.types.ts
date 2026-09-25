524a0ff44cc65e09f820bdd520561815a387cd75
Donald Jose Munguia Guadamuz <jdonaldmunguia@gmail.com>
2026-09-25 09:29:02 -0600

    fixed(#353): resolved: #353 - Reparar generacion de documento de la orden de pago

--- apps/erp-alpac-group/src/modules/purchasing/ui/pages/purchase-order/components/purchase-order-document-modal/purchase-order-document-modal.types.ts
+++ apps/erp-alpac-group/src/modules/purchasing/ui/pages/purchase-order/components/purchase-order-document-modal/purchase-order-document-modal.types.ts
@@ -1,10 +1,10 @@
-import type { GetPurchaseOrderDetailsResponse } from "@app/modules/purchasing/domain/ApiContract/Responses/purchase/get-purchase-order-details-response";
 import type { PurchaseRequestProductInformation } from "@app/modules/purchasing/domain/ApiContract/Responses/purchase/get-purchase-request-details-response";
+import type { PaymentRequestDocumentDetails } from "@app/modules/purchasing/ui/pages/purchase-order/components/reports/payment-request-pdf/map-payment-request-from-purchase-order";
 
 export interface PurchaseOrderDocumentModalProps {
 	isOpen: boolean;
 	onClose: () => void;
 	purchaseOrderId: string;
-	details?: GetPurchaseOrderDetailsResponse | null;
+	details?: PaymentRequestDocumentDetails | null;
 	products?: PurchaseRequestProductInformation[];
 }
